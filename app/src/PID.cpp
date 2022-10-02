37625b20d5d5612e1613f53de539787994ff1194
Anukriti Singh <anukriti@umd.edu>
2022-10-02 16:00:37 -0400

    Update PID.cpp
    Adding comments for the code

--- app/src/PID.cpp
+++ app/src/PID.cpp
@@ -1,4 +1,16 @@
+/*Copyright 2022 Anukriti Singh, Sanchit Tanwar, 
+ Bharadwaj Chukkala & Shantanu Parab */
+
+
+
 #include "PID.hpp"
+
+/**
+ * @brief Starting point for PID execution. Instantiate a PID object with initial and target velocities.
+ * call the function calculate of PID class for 100 iterations
+ * 
+ * @return int 
+ */
 
 PID::PID(double Kp_, double Ki_, double Kd_) {
   Kp = Kp_;
